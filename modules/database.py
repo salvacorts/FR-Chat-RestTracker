70947814083db2c7343067e574994413d143f4ae
Salva Corts <salvacorts97@gmail.com>
2017-10-30 08:53:04 +0100

    SQL Injection preventions on Database

--- modules/database.py
+++ modules/database.py
@@ -1,47 +1,26 @@
 import sqlite3 as sql
 
-class SqlCalls:
-    addUser = """ INSERT INTO users (name, ip, pubKey) VALUES ("{0}", "{1}", "{2}"); """
-    updateUser = """ UPDATE users SET ip="{1}", pubKey="{2}" WHERE name="{0}" """
-    userInfo = """SELECT * FROM users WHERE name="{0}" LIMIT 1"""
-    keyExists = """SELECT count(*) FROM users WHERE name="{0}" """
-
-    @staticmethod
-    def AddUserSql(name, ip, pubKey):
-        return SqlCalls.addUser.format(name, ip, pubKey)
-
-    @staticmethod
-    def UpdateUserSql(name, ip, pubKey):
-        return SqlCalls.updateUser.format(name, ip, pubKey)
-
-    @staticmethod
-    def GetUserInfoSql(name):
-        return SqlCalls.userInfo.format(name)
-
-    @staticmethod
-    def KeyExistsSql(name):
-        return SqlCalls.keyExists.format(name)
-
-
 class DataBase:
+    addUser = """ INSERT INTO users (name, ip, pubKey) VALUES (?, ?, ?); """
+    updateUser = """ UPDATE users SET ip=?, pubKey=? WHERE name=? """
+    userInfo = """SELECT * FROM users WHERE name=? LIMIT 1"""
+    keyExists = """SELECT count(*) FROM users WHERE name=? """
+
     def __init__(self, dbFile):
         self.file = dbFile
         self.db = sql.connect(self.file)
         self.dbCursor = self.db.cursor()
 
     def AddUser(self, name, ip, pubKey):
-        sql = SqlCalls.AddUserSql(name, ip, pubKey)
-        self.dbCursor.execute(sql)
+        self.dbCursor.execute(DataBase.addUser, (name, ip, pubKey,))
         self.db.commit()
 
     def UpdateUser(self, name, ip, pubKey):
-        sql = SqlCalls.UpdateUserSql(name, ip, pubKey)
-        self.dbCursor.execute(sql)
+        self.dbCursor.execute(DataBase.updateUser, (name, ip, pubKey,))
         self.db.commit()
 
     def GetUser(self, name):
-        sql = SqlCalls.GetUserInfoSql(name)
-        self.dbCursor.execute(sql)
+        self.dbCursor.execute(DataBase.userInfo, (name,))
 
         out = self.dbCursor.fetchone()
 
@@ -52,8 +31,7 @@ class DataBase:
         }
 
     def KeyExists(self, name):
-        sql = SqlCalls.KeyExistsSql(name)
-        self.dbCursor.execute(sql)
+        self.dbCursor.execute(DataBase.keyExists, (name,))
 
         if (int(self.dbCursor.fetchone()[0]) > 0):
             return True
